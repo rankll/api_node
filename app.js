e0fd5770d4918e38d2c79ca97322aa4d1a01444e
EC2 Default User <ec2-user@ip-172-31-44-255.sa-east-1.compute.internal>
2015-05-30 17:04:12 +0000

    primeiro commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+
+var app = express();
+
+app.get('/', function(req, res){
+	res.send('Ola Mundo!');
+});
+
+app.get('/things/:nome/:coisa/:local', function(req, res){
+	var things = ["Naruto", "Quintas", "IMD", "Pastelanche"];
+	var thingPass = req.params.nome;
+	var thingPass2 = req.params.coisa;
+	var thingPass3 = req.params.local;
+	console.log('Thing Passado: '+thingPass + ', ' + thingPass2 + ', ' + thingPass3);
+});
+
+var server = app.listen(8080);
