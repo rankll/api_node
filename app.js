3ead062114853792aaf208743ddeb0c949143650
andremiranda <andremirannda@gmail.com>
2015-06-09 20:54:17 -0300

    ajustando app/.js

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ var app = express();
 
 MongoClient.connect('mongodb://localhost:27017/rankll', function(err, db) {
     "use strict";
-    if(err) throw err;    
-    
-    // parse application/json
-    app.use(bodyParser.json());       
+    if(err) throw err;                
 
     // Application routes
-    routes(app, db);
-
+    routes(app, db);        
+    // parse application/json
+    app.use(bodyParser.json());
+    //Instanciando o servidor    
     app.listen(8080);
+    
     console.log('Express server listening on port 8080');
 });
 
